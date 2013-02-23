00b74ca27e42c965f805121aa4db8d3e3f999e5b
David Van Der Beek <earlynovrock@gmail.com>
2013-02-23 11:19:18 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmoStripe::Application.config.secret_token = '9d3d8905523b661f4c7aff12e5b6ef1ccb633f6e2f721d45e7e9aa435639972c83dd1602ab4e336ade7037baec02517e2fbf0ad4c1834defcf6832bb121314ef'
