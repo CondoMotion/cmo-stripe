80098fb92e84288885b4f9d69859b7dcf2b13ca8
David Van Der Beek <earlynovrock@gmail.com>
2013-07-18 13:12:40 -0500

    add temporary solution for filtering news and docs by property

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
 	has_one :post, as: :postable
 	has_one :attachment, as: :attachable
+  has_many :sites, through: :posts
   
   accepts_nested_attributes_for :post, :attachment
 
