7dbe2a0256aa95ff64faf103705d633653abf3c8
David Van Der Beek <earlynovrock@gmail.com>
2013-08-19 14:16:52 -0500

    add vendor to autoload path

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module CmoStripe
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/vendor)
 
 
     # Only load the plugins named here, in the order given (default is alphabetical).
