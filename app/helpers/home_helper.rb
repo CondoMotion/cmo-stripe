918478574633c9858eb84c09f370079d3668a49d
David Van Der Beek <earlynovrock@gmail.com>
2013-08-20 14:48:00 -0500

    add screenshots and descriptions to homepage

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def browser_bar
+    content_tag :div, class: "browser-header" do
+      content_tag(:i, "", class: "icon-arrow-left") +
+      content_tag(:i, "", class: "icon-arrow-right") + 
+      content_tag(:i, "", class: "icon-refresh") +
+      content_tag(:i, "", class: "icon-home") +
+      content_tag(:div, "", class: "red pull-right")
+    end
+  end
 end
