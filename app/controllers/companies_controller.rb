42699c0d948a9efa390a174ff85702b88a076be0
David Van Der Beek <earlynovrock@gmail.com>
2013-08-27 11:17:02 -0500

    add clearfix to users form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,43 +1,43 @@
 class CompaniesController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-	def show
-		@company = current_user.company
-	end
+  def show
+    @company = current_user.company
+  end
 
   def edit
-  	@company = current_user.company
-  	authorize! :edit, @company
+    @company = current_user.company
+    authorize! :edit, @company
   end
 
   def create
-  	@company = current_user.build_owned_company(params[:company])
-  	authorize! :create, @company
-  	if @company.save 
-  		redirect_to edit_company_path, notice: "Company created successfully!"
-  	else
-  		render "edit"
-  	end
+    @company = current_user.build_owned_company(params[:company])
+    authorize! :create, @company
+    if @company.save 
+      redirect_to edit_company_path, notice: "Company created successfully!"
+    else
+      render "edit"
+    end
   end
 
   def update
-  	@company = current_user.company
-  	authorize! :edit, @company
+    @company = current_user.company
+    authorize! :edit, @company
 
-  	if @company.update_attributes(params[:company])
+    if @company.update_attributes(params[:company])
       if params[:redirect_to] == "company"
         redirect_to @company, notice: "Company information updated successfully!"
       else
-  		  redirect_to '/users/edit/company', notice: "Company information updated successfully!"
+        redirect_to '/users/edit/company', notice: "Company information updated successfully!"
       end
-  	else
-  		render "edit"
-  	end
+    else
+      render "edit"
+    end
   end
 
   def users
-  	@users = current_user.company.users
-  	authorize! :index, User
+    @users = current_user.company.users
+    authorize! :index, User
   end
 
 end
